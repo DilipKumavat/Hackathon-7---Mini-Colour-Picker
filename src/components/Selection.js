9e0c64ecd7b4ef0a7303a8420c1de8b26004b89d
DilipKumavat <officialkumavat@gmail.com>
2020-12-04 03:04:31 +0530

    removed finalcolor function

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,19 +1,17 @@
 import React,{useState} from 'react';
 function Selection(props){
     const [color, setcolor] = useState('');
-    const addColor = (bg)=>{
-        
-        return bg.background;
+    const addColor = (bg)=>(evt)=>{
+        setcolor(bg.background);
+        //return bg.background;
         //evt.target.style.background=bg.background;
         //console.log(props.applyColor+" color");
         //console.log(evt);
         //console.log(evt.target.style);
         
     }
-    const finalColor = ()=>{
-        setcolor(props.applyColor(addColor))
-    }
-    return (<div className="fix-box"  style={{background : color}} onClick={finalColor} >
+    
+    return (<div className="fix-box"  style={{background : color}} onClick={props.applyColor(addColor)} >
             <h2 className="subheading">Selection</h2>
         </div>)
 }
